62d91471e64c9cd0ef14cd743c2c4a6cc70053f8
Tarik Onalan <phi.quantum@gmail.com>
2014-09-18 23:08:34 -0700

    Problem 1

---
+++ Problem_1/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main() {
+    int sum = 0;
+    int i;
+    for(i = 3; i < 1000; i++)
+        if(i % 3 == 0 || i % 5 == 0)
+            sum += i;
+    printf("%d", sum);
+}
