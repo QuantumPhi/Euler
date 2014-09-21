52e03ff18b924044950b223ca35522a1c4945946
Tarik Onalan <phi.quantum@gmail.com>
2014-09-20 20:15:59 -0700

    Problem 5

---
+++ Problem_5/main.c
@@ -0,0 +1,18 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+long long gcd(long long a, long long b)
+{
+    return (b == 0 ? a : gcd(b, a % b));
+}
+
+long long lcm(long long a, long long b)
+{
+    return a * b / gcd(a, b);
+}
+
+void main()
+{
+    long long value = lcm(20, lcm(19, lcm(18, lcm(17, lcm(16, lcm(15, lcm(14, lcm(13, lcm(12, lcm(11, lcm(10, lcm(9, lcm(8, lcm(7, lcm(6, lcm(5, lcm(4, lcm(3, lcm(2, 1)))))))))))))))))));
+    printf("%lld", value);
+}
