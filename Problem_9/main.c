d73e1417d3f8a77d54b94016098b4659b4287fd5
Tarik Onalan <phi.quantum@gmail.com>
2014-10-05 13:27:39 -0700

    Grid for Problem 11

--- Problem_9/main.c
+++ Problem_9/main.c
@@ -6,14 +6,8 @@ void main()
     int b = 1;
     int c = 1;
     for(a; a < 1000; a++)
-    {
         for(b = 1; b < a; b++)
-        {
             for(c = 1; c < b; c++)
-            {
                 if(a*a==b*b+c*c && a+b+c == 1000)
                     printf("%d", a*b*c);
-            }
-        }
-    }
 }
